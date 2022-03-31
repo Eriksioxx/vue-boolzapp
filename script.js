96d4da03ae28175cd32112c29cb72e86094b2c0d
erik schievenin <erik.schievenin@gmail.com>
2022-03-31 02:40:41 +0200

    exercise

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const app = new Vue({
     el: '#app',
     data: {
         indexCounter: -1,
+        timeClicked: 0,
+        counterMessage: null,
         personalId: {
             name: 'Erik S.',
             avatar: 'Erik.jpg'
@@ -17,17 +19,17 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 16:15:22',
+                        date: '16:15',
                         message: 'Tutto fatto!',
                         status: 'received'
                     }
@@ -40,17 +42,17 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '20/03/2020 16:30:00',
+                        date: '16:30',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
-                        date: '20/03/2020 16:30:55',
+                        date: '16:30',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
-                        date: '20/03/2020 16:35:00',
+                        date: '16:35',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
                     }
@@ -63,17 +65,17 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '28/03/2020 10:10:40',
+                        date: '10:10',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
-                        date: '28/03/2020 10:20:10',
+                        date: '10:20',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 16:15:22',
+                        date: '16:15',
                         message: 'Ah scusa!',
                         status: 'received'
                     }
@@ -86,12 +88,12 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
                     }
@@ -104,12 +106,12 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
                     }
@@ -122,17 +124,17 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '15:51',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
                     }
@@ -145,12 +147,12 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
                     }
@@ -163,28 +165,41 @@ const app = new Vue({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '15:51',
                         message: 'OK!!',
                         status: 'received'
                     }
                 ],
             }
-        ]
+        ],
+        infoMenu: ['Reply', 'Forward message', 'Important Message', 'Delete Message']
     },
 
     methods: {
         selectContact(index) {
             this.indexCounter = index;
-        }
+            this.counterMessage = null;
+        },
+        selectMessage(index) {
+            if (this.timeClicked == 0) {
+                this.counterMessage = index;
+                this.timeClicked++;
+            } else {
+                this.counterMessage = null;
+                this.timeClicked--;
+            }
+        },
+
+
     }
 })
